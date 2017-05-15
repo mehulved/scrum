7addf6bdf585ef5751bf6b1c2e0b160d298c1054
Mehul Ved <mehul.ved@nexsales.com>
2017-05-15 14:54:37 +0530

    added API endpoints to manage calls and user status

--- scrum.py
+++ scrum.py
@@ -25,8 +25,36 @@ class UserResource:
         user = "{}"
         resp.body = json.dumps(user)
 
+
+
+class CallUserResource:
+    def on_get(self, req, resp, id)
+        """View user call status"""
+        call_user = "{}"
+        resp.body = json.dumps(call_user)
+
+    def on_post(self, req, resp, action, id)
+        """Activate and deactivate calls for user"""
+        call_user = "{}"
+        resp.body = json.dumps(call_user)
+
+
+
+class CallResource:
+    def on_get(self, req, resp)
+        "View the status of scrum call"""
+        call = "{}"
+        resp.body = json.dumps(call)
+
+    def on_post(self, req, resp, action)
+        call = "{}"
+        resp.body = json.dumps(call)
+
+
 api = falcon.API()
 
 # Add the routes
 api.add_route('/users', UsersResource())
 api.add_route('/user/{id}', UserResource())
+api.add_route('/call/user/{id}/{action}', CallUserResource())
+api.add_route('/call/{action}', CallResource())
